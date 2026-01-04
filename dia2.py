8c1086dcfb7a3e3a7d5eb566a68f3b75c6552923
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-04 14:13:02 -0300

    finalizado o dia 2 e criado o arquivo de exercicios do dia 2

---
+++ dia2.py
@@ -0,0 +1,35 @@
+#variaveis - Lugar que armazenamos dados na memoria do computador
+
+idade = 23 #inteiro
+peso = 65.5 #real
+nome = 'Fernanda' #string
+casado = False #booleano
+altura = 1.70 #real
+
+
+print(type(nome))
+print(type(idade))
+print(type(peso))
+print(type(casado))
+
+
+
+#tipos de dados
+#inteiro - int
+#real - float
+#string - str
+#booleano - bool
+
+
+print(5 + 3) #soma
+print (10 - 2) #subtracao
+print(4 * 2) #multiplicacao
+print(16 / 2) #divisao
+print(5 // 2) #divisao inteira
+print(5 % 2) #resto da divisao
+print(2 ** 3) #exponenciacao
+
+
+nome = "Ana"
+sobrenome = "Silva"
+print( "Ola meu nome é :", nome + ' ' + sobrenome)

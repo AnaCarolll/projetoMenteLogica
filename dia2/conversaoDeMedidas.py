0443be123f62f5fc6d4a256fc4f93d25a25cd196
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-06 18:11:56 -0300

    criado o script que converte metida

---
+++ dia2/conversaoDeMedidas.py
@@ -0,0 +1,8 @@
+infoMedida = float(input("Digite o valor em metros: "))
+centimetros = infoMedida * 100
+milimetros = infoMedida * 1000
+
+infoMedidaKm = infoMedida / 1000
+print(f"O valor em centimetros é: {centimetros} cm")
+print(f"O valor em milimetros é: {milimetros} mm")
+print(f"O valor em quilômetros é: {infoMedidaKm} km")

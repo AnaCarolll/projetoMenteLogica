828cb2dae47a5de9c6eaa5ad358c597b9d7a37aa
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-06 18:00:06 -0300

    criado o arquivo que contem o codigo da calculadora

---
+++ dia2/calculadora.py
@@ -0,0 +1,18 @@
+# meuNome= 'Ana carolina'
+# frase = "Olá, meu nome é: " + meuNome + " prazer em te conhecer!"
+
+# print(frase)
+
+#calculadora simples
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("Soma: ", soma)
+print("Subtração: ", subtracao)
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)

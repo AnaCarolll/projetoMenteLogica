80279a99d5f1dd62237342caa845bf5b30db33e5
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-06 19:02:18 -0300

    criado o scritp completo da aula 4

--- dia3/index.py
+++ dia3/index.py
@@ -1,7 +1,55 @@
 # ** -  operadores de exponenciação
 # / / - divisão inteira
+# / - resto da divisão
+# % - modulo
 
 a = 10
 b = 5
 
 print(a // b)
+print(a % b)
+print(a ** b)
+
+# operadores de comparação
+# >, <, >=, <=, ==, !=
+
+x = 20
+y = 15
+
+print(x > y)
+print(x <= y)
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x < y)
+
+
+# operadores lógicos
+# and, or, not
+# and - todas as condições devem ser verdadeiras
+# or - pelo menos uma condição deve ser verdadeira
+# not - inverte o valor lógico
+
+c = 8
+d = 12
+print(c < 10 and d > 10)
+print(c < 10 or d < 10)
+print(not(c < 10))
+
+# operadores de atribuição
+# =, +=, -=, *=, /=, //=, **=, %=
+
+num = 10
+num += 5
+print(num)
+num *= 2
+print(num)
+num %= 6
+print(num)
+num **= 3
+print(num)
+num //= 4
+print(num)
+num -= 3
+print(num)
+

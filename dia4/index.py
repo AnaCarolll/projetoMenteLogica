9926cdb39987ebc2eccef408c914cdf400cd51b1
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-19 08:28:25 -0300

    subindo arquivo da aula quatro

---
+++ dia4/index.py
@@ -0,0 +1,25 @@
+# Condicionais -> estrutura if, else
+
+# idade = 7
+
+# if idade > 18:
+#     {
+#         print('voce é maior de idade')
+#     }
+# else: {
+# print('voce é menor de idade')
+# }
+    
+#colocando mais robustes
+
+suaIdade = float(input('qual é a sua idade?'))
+
+if suaIdade > 18:
+    {
+        print('voce é maior de idade')
+    }
+else: {
+print('voce é menor de idade')
+
+}
+  

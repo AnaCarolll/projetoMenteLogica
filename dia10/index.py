93787d55457e279dcde5f2bf82f05d386acdd3da
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-25 18:31:39 -0300

    subindo pasta sobre modulo

---
+++ dia10/index.py
@@ -0,0 +1,13 @@
+#moduloos 
+#pacotes que podem estar ou Nao instalados n pYTHN
+#ESTAO = importa
+#NAO ESTAO = instalar com gerenciador de pacotes pip
+
+import math
+
+valor = 16
+raiz = math.sqrt(valor)
+print(f"A raiz quadrada de {valor} é {raiz}")
+
+from math import pi
+print(f"O valor de pi é {pi}")

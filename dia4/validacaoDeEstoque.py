94b0157e6ec9e5085d5667899222bc973ed1e388
Ana Carolina <anacarolsantiago2005@gmail.com>
2026-01-25 08:09:02 -0300

    subindo atividades do dia 04

---
+++ dia4/validacaoDeEstoque.py
@@ -0,0 +1,15 @@
+# agenda = 4
+# if(agenda > 0) :
+#     print("Temos linha em estoque")
+
+diasDisponiveis = ['segunda', 'terça', 'quarta', 'quinta', 'sexta']
+for dia in diasDisponiveis:
+    print(f"Temos agenda disponível na {dia}")
+
+
+
+tempoDeUsoCelular = 3
+if(tempoDeUsoCelular < 2):
+    print("Bateria em bom estado")
+else:
+    print("Considere trocar a bateria do celular")
